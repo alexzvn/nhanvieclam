056d9b9d05778875a6c482c6ab5113b83239f15b
Alexzvn <alex.vn.9991@gmail.com>
2022-02-21 18:45:55 +0700

    add authorize for category

--- app/Http/Controllers/Manager/CategoryController.php
+++ app/Http/Controllers/Manager/CategoryController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    public function __construct() {
+        $this->authorizeResource(Category::class, 'category');
+    }
+
     /**
      * Display a listing of the resource.
      *
